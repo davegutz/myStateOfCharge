bad038c3c7eefdd7379cacadd985046f12388fd7
Dave G <davegutz@alum.mit.edu>
2021-08-25 08:25:11 -0400

    debug

--- SOC_Photon/src/Battery.h
+++ SOC_Photon/src/Battery.h
@@ -103,8 +103,8 @@ const unsigned int nz_bb = 7;
 // Charge test profiles
 #define NUM_VEC           1   // Number of vectors defined here
 static const unsigned int n_v1 = 10;
-static const double t_min_v1[n_v1] =  {0,     0.2,   0.2001, 1.4,   1.4001, 2.4999, 2.5,    2.6999, 2.7,    3.5};
-static const double v_v1[n_v1] =      {13.65, 13.65, 13.65,  11.8,  11.8,   11.8,   11.8,   13.65,  13.65,  13.65};
+static const double t_min_v1[n_v1] =  {0,     0.2,   0.2001, 1.4,   1.4001, 2.4999, 2.5,    3.6999, 3.7,    4.5};
+static const double v_v1[n_v1] =      {13.65, 13.65, 13.65,  12.2,  12.2,   12.2,   12.2,   13.65,  13.65,  13.65};
 static const double i_v1[n_v1] =      {0.,    0.,    -500.,  -500., 0.,     0.,     500.,   500.,   0.,     0.};
 static const double T_v1[n_v1] =      {72.,   72.,   72.,    72.,   72.,    72.,    72.,    72.,    72.,    72.};
 static TableInterp1Dclip  *V_T1 = new TableInterp1Dclip(n_v1, t_min_v1, v_v1);
