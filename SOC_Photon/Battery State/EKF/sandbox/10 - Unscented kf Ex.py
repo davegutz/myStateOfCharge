b7345490d60ea0c285b65373629ef95a6336e85c
Dave G <davegutz@alum.mit.edu>
2021-11-13 08:27:13 -0500

    progress

---
+++ SOC_Photon/Battery State/EKF/sandbox/10 - Unscented kf Ex.py
@@ -0,0 +1,9 @@
+from kf_book.book_plots import set_figsize, figsize
+import matplotlib.pyplot as plt
+from kf_book.nonlinear_plots import plot_nonlinear_func
+from numpy.random import normal
+import numpy as np
+import kf_book.ukf_internal as ukf_internal
+
+import book_format
+book_format.set_style()
