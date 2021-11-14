2c4bec5e5b38cbadf5bd9fd08a80bb5a216d6552
Dave G <davegutz@alum.mit.edu>
2021-11-14 07:44:07 -0500

    play

---
+++ SOC_Photon/Battery State/EKF/sandbox/10 - Unscented kf Ex Robot.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from filterpy.common import Q_discrete_white_noise
+from numpy.random import randn
+from filterpy.kalman import UnscentedKalmanFilter
+from filterpy.kalman import JulierSigmaPoints
+from filterpy.kalman import UnscentedKalmanFilter as UKF
+import kf_book.ukf_internal as ukf_internal
+from filterpy.kalman import unscented_transform, MerweScaledSigmaPoints
+from numpy.linalg import norm
+import math
+from math import atan2
+
+import book_format
+book_format.set_style()
+
+
+from math import tan, sin, cos, sqrt
+
+def move(x, dt, u, wheelbase):
+    hdg = x[2]
+    vel = u[0]
+    steering_angle = u[1]
+    dist = vel * dt
+
+    if abs(steering_angle) > 0.001: # is robot turning?
+        beta = (dist / wheelbase) * tan(steering_angle)
+        r = wheelbase / tan(steering_angle) # radius
+
+        sinh, sinhb = sin(hdg), sin(hdg + beta)
+        cosh, coshb = cos(hdg), cos(hdg + beta)
+        return x + np.array([-r*sinh + r*sinhb,
+                              r*cosh - r*coshb, beta])
+    else: # moving in straight line
+        return x + np.array([dist*cos(hdg), dist*sin(hdg), 0])
+
+def normalize_angle(x):
+    x = x % (2 * np.pi)    # force in range [0, 2 pi)
+    if x > np.pi:          # move to [-pi, pi)
+        x -= 2 * np.pi
+    return x
+
+def residual_h(a, b):
+    y = a - b
+    # data in format [dist_1, bearing_1, dist_2, bearing_2,...]
+    for i in range(0, len(y), 2):
+        y[i + 1] = normalize_angle(y[i + 1])
+    return y
+
+def residual_x(a, b):
+    y = a - b
+    y[2] = normalize_angle(y[2])
+    return y
+
+def Hx(x, landmarks):
+    """ takes a state variable and returns the measurement
+    that would correspond to that state. """
+    hx = []
+    for lmark in landmarks:
+        px, py = lmark
+        dist = sqrt((px - x[0])**2 + (py - x[1])**2)
+        angle = atan2(py - x[1], px - x[0])
+        hx.extend([dist, normalize_angle(angle - x[2])])
+    return np.array(hx)
+
