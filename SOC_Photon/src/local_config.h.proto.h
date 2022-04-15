25d5b420fe8e985ffbcf1d3835c150a7d7d3cbd6
Dave G <davegutz@users.noreply.github.com>
2022-04-14 20:49:42 -0400

    stat

--- SOC_Photon/src/local_config.h.proto.h
+++ SOC_Photon/src/local_config.h.proto.h
@@ -1,7 +1,7 @@
 #ifndef local_config_h
 #define local_config_h
 
-const   String    unit = "proto_20220410";
+const   String    unit = "proto_20220414";
 #define CURR_BIAS_AMP       -12.233
 #define CURR_BIAS_NOAMP     -12.041
 #define CURR_BIAS_ALL        0.0
