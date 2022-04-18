48e711ff3db240822c8c12e69e78babf38354864
Dave G <davegutz@alum.mit.edu>
2022-04-18 12:00:53 -0400

    Update local_config.h.proto.h

--- SOC_Photon/src/local_config.h.proto.h
+++ SOC_Photon/src/local_config.h.proto.h
@@ -1,9 +1,9 @@
 #ifndef local_config_h
 #define local_config_h
 
-const   String    unit = "proto_20220414";
-#define CURR_BIAS_AMP       -12.233
-#define CURR_BIAS_NOAMP     -12.041
+const   String    unit = "proto_20220418";
+#define CURR_BIAS_AMP       -3.0
+#define CURR_BIAS_NOAMP     -0.7
 #define CURR_BIAS_ALL        0.0
 
 #endif
