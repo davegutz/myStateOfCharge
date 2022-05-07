d21e05bf465a0fbe9b3ccb0129b0bb082433b893
Dave G <davegutz@alum.mit.edu>
2022-05-07 11:50:32 -0400

    huh

--- SOC_Photon/src_old/SOC_Photon.ino
+++
@@ -1,2 +0,0 @@
-// main.h is used so editors visual studio reliably applies file associations.   main.cpp does not work
-#include "main.h"
--- SOC_Photon/src_old/myAuth.h
+++
@@ -1,8 +0,0 @@
-#ifndef myAuth_h
-#define myAuth_h
-#ifndef BARE_PHOTON
-    const   String    blynkAuth  = "DU9igmWDh6RuwYh6QAI_fWsi-KPkb7Aa"; // SOC_Proto
-#else
-    const   String    blynkAuth  = "DU9igmWDh6RuwYh6QAI_fWsi-KPkb7Aa"; // SOC_Proto
-#endif
-#endif
