e6612fa6865b16fefc3a4eb82309c3d4a1c9a930
Dave G <davegutz@alum.mit.edu>
2022-06-18 14:45:19 -0400

    Update main.h

--- SOC_Photon/src/main.h
+++ SOC_Photon/src/main.h
@@ -59,7 +59,7 @@
   #undef min
 #endif
 
-#define USE_BT             // Change this to #define to use Bluetooth
+#undef USE_BT             // Change this to #define to use Bluetooth
 
 #include "constants.h"
 
@@ -465,7 +465,7 @@ void loop()
   // Publish1 Blynk
   void publish1(void)
   {
-    if (rp.debug>104) Serial.printf("Blynk write1\n");
+    if (rp.debug==25) Serial.printf("Blynk write1\n");
     Blynk.virtualWrite(V2,  pp.pubList.Vbatt);
     Blynk.virtualWrite(V3,  pp.pubList.Voc);
     Blynk.virtualWrite(V4,  pp.pubList.Vbatt);
@@ -475,7 +475,7 @@ void loop()
   // Publish2 Blynk
   void publish2(void)
   {
-    if (rp.debug>104) Serial.printf("Blynk write2\n");
+    if (rp.debug==25) Serial.printf("Blynk write2\n");
     Blynk.virtualWrite(V6,  pp.pubList.soc);
     Blynk.virtualWrite(V8,  pp.pubList.T);
     Blynk.virtualWrite(V10, pp.pubList.Tbatt);
@@ -485,7 +485,7 @@ void loop()
   // Publish3 Blynk
   void publish3(void)
   {
-    if (rp.debug>104) Serial.printf("Blynk write3\n");
+    if (rp.debug==25) Serial.printf("Blynk write3\n");
     Blynk.virtualWrite(V15, pp.pubList.hm_string);
     Blynk.virtualWrite(V16, pp.pubList.tcharge);
   }
@@ -494,7 +494,7 @@ void loop()
   // Publish4 Blynk
   void publish4(void)
   {
-    if (rp.debug>104) Serial.printf("Blynk write4\n");
+    if (rp.debug==25) Serial.printf("Blynk write4\n");
     Blynk.virtualWrite(V18, pp.pubList.Ibatt);
     Blynk.virtualWrite(V20, pp.pubList.Wbatt);
     Blynk.virtualWrite(V21, pp.pubList.soc_ekf);
