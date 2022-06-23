b64561b76d5eb7e2863f98f2e57dd55a317204d7
Dave G <davegutz@alum.mit.edu>
2022-06-23 18:09:44 -0400

    reorg for android

--- SOC_Photon/py/SOC_Photon_USB.py
+++ SOC_Photon/py/SOC_Photon_USB.py
@@ -19,8 +19,9 @@ from pylive import liven_plotter
 import matplotlib.pyplot as plt
 from SOC_Photon_BLE import plot_SOC_Photon_data
 
+
 com_port = 'COM4'
 key = 'pro_'
 
 s = serial.Serial(port=com_port, baudrate=115200,  bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
-plot_SOC_Photon_data(s)
+plot_SOC_Photon_data(s, key)
