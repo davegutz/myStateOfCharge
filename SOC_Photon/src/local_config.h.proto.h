69062951db03fd914620e69e2308e61b17aa34ca
Dave G <davegutz@users.noreply.github.com>
2022-07-23 04:14:25 -0400

    regress

--- SOC_Photon/src/local_config.h.proto.h
+++ SOC_Photon/src/local_config.h.proto.h
@@ -1,7 +1,7 @@
 #ifndef local_config_h
 #define local_config_h
 
-const String unit = "pro_20220718";  // Init ekf
+const String unit = "pro_20220723";  // Signal select
 
 // Sensor biases
 #define CURR_BIAS_AMP         -0.24 // Calibration of amplified shunt sensor ('Da=#.#;'), A
