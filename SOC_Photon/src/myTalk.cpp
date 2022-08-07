ae674b4635d78df5a06edba9fb8b3dd1e117393b
Dave G <davegutz@users.noreply.github.com>
2022-08-07 08:05:40 -0400

    Update myTalk.cpp
    Xm being set to Xm7 after beginning Xp10 with Xm15.   Add tweak_test before "Xm7" in Xp0

--- SOC_Photon/src/myTalk.cpp
+++ SOC_Photon/src/myTalk.cpp
@@ -864,7 +864,7 @@ void talk(BatteryMonitor *Mon, Sensors *Sen)
                     break;
 
                   case ( 0 ):  // Xp0:  reset stop
-                    chit("Xm7;", ASAP);
+                    if ( !rp.tweak_test() ) chit("Xm7;", ASAP);
                     chit("Xf0.; Xa0.", ASAP);
                     if ( !rp.tweak_test() ) chit("Xb0.", ASAP);
                     chit("XS; Mk1; Nk1;", ASAP);  // Stop any injection
