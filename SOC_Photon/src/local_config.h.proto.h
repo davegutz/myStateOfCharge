ed2c5a77b3ceed23bc9176ac9a24133d370a373f
Dave G <davegutz@users.noreply.github.com>
2022-09-01 05:57:31 -0400

    tune for real world

--- SOC_Photon/src/local_config.h.proto.h
+++ SOC_Photon/src/local_config.h.proto.h
@@ -1,7 +1,7 @@
 #ifndef local_config_h
 #define local_config_h
 
-const String unit = "pro_20220829";  // Disable e_wrap saturated; tweak, history falw, cal noa, 5 A ibdif, print stuff
+const String unit = "pro_20220901";  // Disable e_wrap saturated; tweak, history falw, cal noa, 5 A ibdif, print stuff, thresh tune
 
 // * = SRAM EEPROM adjustments, retained on power reset
 
