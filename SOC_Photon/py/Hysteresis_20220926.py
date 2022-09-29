14a8e0be3eda2bda45b98aeed280a868490b14af
Dave G <davegutz@users.noreply.github.com>
2022-09-29 13:38:06 -0400

    debug

--- SOC_Photon/py/Hysteresis_20220926.py
+++ SOC_Photon/py/Hysteresis_20220926.py
@@ -26,7 +26,7 @@ from unite_pictures import cleanup_fig_files
 class Hysteresis_20220926:
     # Use variable resistor to create hysteresis from an RC circuit
 
-    def __init__(self, t_dv=None, t_soc=None, t_r=None, cap=3.6e5, scale=1., dv_hys=0.0):
+    def __init__(self, t_dv=None, t_soc=None, t_r=None, cap=3.6e4, scale=1., dv_hys=0.0):
         # Defaults
         if t_dv is None:
             t_dv = [-0.9, -0.7,   -0.5,  -0.3,  0.0,   0.15,   0.3,   0.7,   0.9]
