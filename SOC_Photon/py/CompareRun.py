c96a31aea713a68cddc4e873956f63eb3486f4c1
Dave G <davegutz@users.noreply.github.com>
2022-10-30 12:50:10 -0400

    update tests

--- SOC_Photon/py/CompareRun.py
+++ SOC_Photon/py/CompareRun.py
@@ -61,13 +61,13 @@ if __name__ == '__main__':
         # data_file_old_txt = '../dataReduction/real world Xp20 20220902.txt'; unit_key = 'soc0_2022'; use_ib_mon_in=True; scale_in=1.12
 
         # Regression suite
-        # data_file_old_txt = 'ampHiFail v20221028.txt'
+        data_file_old_txt = 'ampHiFail v20221028.txt'
         # data_file_old_txt = 'ampLoFail v20221028.txt'
         # data_file_old_txt = 'ampHiFailNoise v20221028.txt'
         # data_file_old_txt = 'rapidTweakRegression v20221028.txt'
         # data_file_old_txt = 'rapidTweakRegression40C v20221028.txt'
         # data_file_old_txt = 'slowTweakRegression v20221028.txt'
-        data_file_old_txt = 'triTweakDisch v20221028.txt'
+        # data_file_old_txt = 'triTweakDisch v20221028.txt'
         # data_file_old_txt = 'satSit20220926.txt'
         # data_file_old_txt = 'offSitHysBms20220926.txt'; #time_end_in=50
         # data_file_old_txt = 'offSitHysBmsNoise20220926.txt'; #time_end_in=50
