9d5c403120cd248ca83cc0ed9983b78ccea66821
Dave G <davegutz@users.noreply.github.com>
2023-01-05 05:49:44 -0500

    Swap soc and pro argons - just rename them

--- SOC_Photon/src/local_config.h.soc1a.h
+++ SOC_Photon/src/local_config.h.soc1a.h
@@ -7,21 +7,29 @@ const String unit = "soc1a_20221220";  // voc_stat
 
 // Miscellaneous
 #define ASK_DURING_BOOT       1   // Flag to ask for application of this file to * retained adjustements
-#define MODELING              247 // Nominal modeling bitmap (* 'Xm'), 0=all hdwe, 1+=Tb, 2+=Vb, 4+=Ib, 7=all model.  +240 for discn 
+#define MODELING              247 // Nominal modeling bitmap (* 'Xm'), 0=all hdwe, 1+=Tb, 2+=Vb, 4+=Ib, 7=all model.  +240 for discn    
 #undef USE_BLE
 
 // Sensor biases
-#undef USE_ADS                 // Instead of using ADS device to read amp by I2C, use analog pins
-#define CURR_BIAS_AMP         0.5   // Calibration of amplified shunt sensor (* 'DA'), A
-#define CURR_SCALE_AMP        0.995 // Hardware to match data (* 'SB')
-#define CURR_BIAS_NOA         0.5   // Calibration of non-amplified shunt sensor (* 'DB'), A
-#define CURR_SCALE_NOA        0.985 // Hardware to match data (* 'SA')
-#define SHUNT_GAIN            1333. // Shunt V2A gain (scale with * 'SG'), A/V (1333 is 100A/0.075V)
-#define SHUNT_AMP_R1          5100.     // Amplifed shunt ADS resistance, ohms
+#define USE_ADS                 // Instead of using ADS device to read amp by I2C, use analog pins
+#ifdef USE_ADS
+    #define CURR_BIAS_AMP         -6.152 // Calibration of amplified shunt sensor (* 'DA'), A
+    #define CURR_SCALE_AMP        1.    // Hardware to match data (* 'SB')
+    #define CURR_BIAS_NOA         0.    // Calibration of non-amplified shunt sensor (* 'DB'), A
+    #define CURR_SCALE_NOA        1.    // Hardware to match data (* 'SA')
+    #define SHUNT_GAIN            -1333.// Shunt V2A gain (scale with * 'SG'), A/V (-1333 is -100A/0.075V)
+#else
+    #define CURR_BIAS_AMP         0.    // Calibration of amplified shunt sensor (* 'DA'), A
+    #define CURR_SCALE_AMP        1.    // Hardware to match data (* 'SB')
+    #define CURR_BIAS_NOA         0.    // Calibration of non-amplified shunt sensor (* 'DB'), A
+    #define CURR_SCALE_NOA        1.    // Hardware to match data (* 'SA')
+    #define SHUNT_GAIN            1333. // Shunt V2A gain (scale with * 'SG'), A/V (1333 is 100A/0.075V)
+#endif
+#define SHUNT_AMP_R1          5600.     // Amplifed shunt ADS resistance, ohms
 #define CURR_BIAS_ALL         0.0   // Bias on all shunt sensors (* 'Di'), A
-#define VOLT_BIAS             8.0   // Bias on Vb sensor (* 'Dc'), V
+#define VOLT_BIAS             1.8   // Bias on Vb sensor (* 'Dc'), V
 #define TEMP_BIAS             0.0   // Bias on Tb sensor (* 'Dt'), deg C
-#define VB_SCALE              0.9877 // Scale Vb sensor (* 'SV')
+#define VB_SCALE              1.0   // Scale Vb sensor (* 'SV')
 
 // Battery.  One 12 V 100 Ah battery bank would have RATED_BATT_CAP 100, NS 1, and NP 1
 // Two 12 V 100 Ah series battery bank would have RATED_BATT_CAP 100, NS 2, and NP 1
