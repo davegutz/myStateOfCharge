600e73bf991fcb7f725be96cd3f11c9b859d2f8e
Dave G <davegutz@users.noreply.github.com>
2023-02-07 11:56:23 -0500

    v20230207 again

--- SOC_Photon/src/local_config.h.soc1a.h
+++ SOC_Photon/src/local_config.h.soc1a.h
@@ -1,7 +1,7 @@
 #ifndef local_config_h
 #define local_config_h
 
-const String unit = "soc1a_20230205";  // voc(soc)
+const String unit = "soc1a_20230207";  // voc(soc)
 
 // * = SRAM EEPROM adjustments, retained on power reset
 
