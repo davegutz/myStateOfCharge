153445721c65fe2b7c0d9bcf56753dd91c4a6e50
Dave G <davegutz@users.noreply.github.com>
2023-05-19 07:17:23 -0400

    Update TestSOC.py

--- SOC_Particle/py/TestSOC.py
+++ SOC_Particle/py/TestSOC.py
@@ -40,7 +40,7 @@ sel_list = ['ampHiFail', 'rapidTweakRegression', 'offSitHysBms', 'triTweakDisch'
             'offSitHysBmsNoise', 'ampHiFailSlow', 'vHiFail', 'vHiFailFf', 'pulseEKF', 'pulseSS', 'tbFailMod',
             'tbFailHdwe']
 lookup = {'ampHiFail': ('Ff0;D^0;Xm247;Ca0.5;Dr100;DP1;HR;Pf;v2;W30;Dm50;Dn0.0001;', 'Hs;Hs;Hs;Hs;Pf;DT0;DV0;DM0;DN0;Xp0;Rf;W200;+v0;Ca.5;Dr100;Rf;Pf;DP4;', ("Should detect and switch amp current failure (reset when current display changes from '50/diff' back to normal '0' and wait for CoolTerm to stop streaming.)", "'diff' will be displayed. After a bit more, current display will change to 0.", "To evaluate plots, start looking at 'DOM 1' fig 3. Fault record (frozen). Will see 'diff' flashing on OLED even after fault cleared automatically (lost redundancy).", 'ev4')),
-          'rapidTweakRegression': ('Ff0;HR;Xp10;', 'self terminates', ('',)),
+          'rapidTweakRegression': ('Ff0;HR;Xp10;', 'self terminates', ('Should run three very large current discharge/recharge cycles without fault', 'Best test for seeing time skews and checking fault logic for false trips')),
           'offSitHysBms': ('Ff0;D^0;Xp0;Xm247;Ca0.05;Rb;Rf;Dr100;DP1;Xts;Xa-162;Xf0.004;XW10;XT10;XC2;W2;Ph;HR;Pf;v2;W5;XR;', 'XS;v0;Hd;Xp0;Ca.05;W5;Pf;Rf;Pf;v0;DP4;', ('',)),
           'triTweakDisch': ('Ff0;D^0;Xp0;v0;Xm15;Xtt;Ca1.;Ri;Mw0;Nw0;MC0.004;Mx0.04;NC0.004;Nx0.04;Mk1;Nk1;-Dm1;-Dn1;DP1;Rb;Pa;Xf0.02;Xa-29500;XW5;XT5;XC3;W2;HR;Pf;v2;W2;Fi1000;Fo1000;Fc1000;Fd1000;FV1;FI1;FT1;XR;', 'v0;Hd;XS;Dm0;Dn0;Fi1;Fo1;Fc1;Fd1;FV0;FI0;FT0;Xp0;Ca1.;Pf;DP4;', ('',)),
           'coldStart': ('Ff0;D^-18;Xp0;Xm247;Fi1000;Fo1000;Ca0.93;Ds0.06;Sk0.5;Rb;Rf;Dr100;DP1;v2;W100;DI40;Fi1;Fo1;', 'DI0;W10;v0;W5;Pf;Rf;Pf;v0;DP4;D^0;Ds0;Sk1;Fi1;Fo1;Ca0.93;', ('',)),
@@ -177,10 +177,10 @@ def lookup_start():
     reset_button.config(text=reset.get())
     while len(ev_val) < 4:
         ev_val = ev_val + ('',)
-    ev1_label.config(text=ev_val[0])
-    ev2_label.config(text=ev_val[1])
-    ev3_label.config(text=ev_val[2])
-    ev4_label.config(text=ev_val[3])
+    ev1_label.config(text='-' + ev_val[0])
+    ev2_label.config(text='-' + ev_val[1])
+    ev3_label.config(text='-' + ev_val[2])
+    ev4_label.config(text='-' + ev_val[3])
 
 
 def show_option(*args):
