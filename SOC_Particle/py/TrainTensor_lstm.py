c8a1b52fd80831626b0827c8e552097e9365d26a
Dave G <davegutz@users.noreply.github.com>
2023-08-13 03:29:52 -0400

    Update TrainTensor_lstm.py

--- SOC_Particle/py/TrainTensor_lstm.py
+++ SOC_Particle/py/TrainTensor_lstm.py
@@ -30,19 +30,25 @@ pd.options.display.max_rows = 10
 pd.options.display.float_format = "{:.1f}".format
 
 
-def create_LSTM(hidden_units, dense_units, input_shape, activation, learning_rate=0.01, dropping=0.2, use_two=False):
+def create_lstm(hidden_units, input_shape, dense_units=1, activation=None, learn_rate=0.01, drop=0.2,
+                use_two_lstm=False):
+    if activation is None:
+        activation = ['relu', 'sigmoid', 'linear']
     lstm = Sequential()
-    if use_two:
-        lstm.add(LSTM(hidden_units, input_shape=input_shape, activation=activation[0], recurrent_activation=activation[1], return_sequences=True))
+    if use_two_lstm:
+        lstm.add(LSTM(hidden_units, input_shape=input_shape, activation=activation[0],
+                      recurrent_activation=activation[1], return_sequences=True))
     else:
-        lstm.add(LSTM(hidden_units, input_shape=input_shape, activation=activation[0], recurrent_activation=activation[1], return_sequences=False))
-    lstm.add(Dropout(dropping))
-    if use_two:
-        lstm.add(LSTM(hidden_units, activation=activation[0], recurrent_activation=activation[1], return_sequences=False))
-        lstm.add(Dropout(dropping))
+        lstm.add(LSTM(hidden_units, input_shape=input_shape, activation=activation[0],
+                      recurrent_activation=activation[1], return_sequences=False))
+    lstm.add(Dropout(drop))
+    if use_two_lstm:
+        lstm.add(LSTM(hidden_units, activation=activation[0],
+                      recurrent_activation=activation[1], return_sequences=False))
+        lstm.add(Dropout(drop))
     lstm.add(Dense(units=dense_units, activation=activation[2]))
     # lstm.compile(loss='mean_squared_error', optimizer='adam')
-    lstm.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate))
+    lstm.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(learning_rate=learn_rate))
     lstm.summary()
     return lstm
 
@@ -69,11 +75,11 @@ def plot_the_loss_curve(epochs_, mse_training, mse_validation):
 
 # Plot the inputs
 def plot_input(trn_x, val_x, tst_x):
-    input = np.append(trn_x, val_x)
-    input = np.append(input, tst_x)
-    rows = len(input)
+    inp = np.append(trn_x, val_x)
+    inp = np.append(inp, tst_x)
+    rows = len(inp)
     plt.figure(figsize=(15, 6), dpi=80)
-    plt.plot(range(rows), input)
+    plt.plot(range(rows), inp)
     plt.axvline(x=len(trn_x), color='r')
     plt.axvline(x=len(trn_x)+len(val_x), color='r')
     plt.legend(['Inputs'])
@@ -125,18 +131,18 @@ def process_battery_attributes(df):
 
 
 # Resize and convert to numpy
-def resizer(v, targ_rows, batch_size, subsample=1):
-    v = v.to_numpy('float')[:targ_rows*batch_size*subsample]
+def resizer(v, targ_rows, btch_size, sub_samp=1):
+    v = v.to_numpy('float')[:targ_rows*btch_size*sub_samp]
     rows_raw = len(v)
-    x = v.reshape(int(rows_raw/subsample), subsample, 1)[:, 0, 0]
-    return x.reshape(targ_rows, batch_size, 1)
+    x = v.reshape(int(rows_raw/sub_samp), sub_samp, 1)[:, 0, 0]
+    return x.reshape(targ_rows, btch_size, 1)
 
 
-def train_model(mod, x, y, epochs_=20, batch_size=1, verbose=0):
+def train_model(mod, x, y, epochs_=20, btch_size=1, verbose=0):
     """Feed a dataset into the model in order to train it."""
 
     # train the model
-    hist = mod.fit(x=x, y=y, epochs=epochs_, batch_size=batch_size, shuffle=False, verbose=verbose)
+    hist = mod.fit(x=x, y=y, epochs=epochs_, batch_size=btch_size, shuffle=False, verbose=verbose)
 
     # Get details that will be useful for plotting the loss curve.
     epochs_ = hist.epoch
@@ -146,21 +152,26 @@ def train_model(mod, x, y, epochs_=20, batch_size=1, verbose=0):
 
 
 # Load data and normalize
+# train_file = ".//temp//dv_train_soc0p_ch_rep.csv"
+# test_file = ".//temp//dv_test_soc0p_ch_rep.csv"
+train_file = ".//generateDV_Data.csv"
+val_fraction = 0.2
+test_file = ".//generateDV_Data.csv"
+
 print("[INFO] loading train/validation attributes...")
-# train_df = pd.read_csv(".//temp//dv_train_soc0p_ch_rep.csv", skipinitialspace=True)
-train = pd.read_csv(".//generateDV_Data.csv", skipinitialspace=True)
+train = pd.read_csv(train_file, skipinitialspace=True)
 train['voc'] = train['vb'] - train['dv_dyn']
 train['dv'] = train['voc'] - train['voc_stat']
 train_df = train[['Tb', 'ib', 'soc', 'dv']]
+
 print("[INFO] loading test attributes...")
-# test_df = pd.read_csv(".//temp//dv_test_soc0p_ch_rep.csv", skipinitialspace=True)
 test = pd.read_csv(".//generateDV_Data.csv", skipinitialspace=True)
 test['voc'] = test['vb'] - test['dv_dyn']
 test['dv'] = test['voc'] - test['voc_stat']
 test_df = test[['Tb', 'ib', 'soc', 'dv']]
 
 # Split training data into train and validation data frames
-train_attr, validate_attr = train_test_split(train_df, test_size=0.2, shuffle=False)
+train_attr, validate_attr = train_test_split(train_df, test_size=val_fraction, shuffle=False)
 test_attr, test_val_attr = train_test_split(test_df, test_size=None, shuffle=False)
 train_attr, train_y = process_battery_attributes(train_attr)
 validate_attr, validate_y = process_battery_attributes(validate_attr)
@@ -174,28 +185,32 @@ dropping = 0.2
 use_two = False
 learning_rate = 0.0001
 epochs = 50
-if use_two:
-    learning_rate *= 2
 hidden = 4
 subsample = 5
-batch_size = int(30 / subsample)
+nom_batch_size = 30
 
+# Adjust model automatically
+if use_two:
+    learning_rate *= 2
+batch_size = int(nom_batch_size / subsample)
+
+# Setup model
 rows_train = int(len(train_y) / batch_size / subsample)
-train_x = resizer(train_x, rows_train, batch_size, subsample=subsample)
-train_y = resizer(train_y, rows_train, batch_size, subsample=subsample)
+train_x = resizer(train_x, rows_train, batch_size, sub_samp=subsample)
+train_y = resizer(train_y, rows_train, batch_size, sub_samp=subsample)
 rows_val = int(len(validate_y) / batch_size / subsample)
-validate_x = resizer(validate_x, rows_val, batch_size, subsample=subsample)
-validate_y = resizer(validate_y, rows_val, batch_size, subsample=subsample)
+validate_x = resizer(validate_x, rows_val, batch_size, sub_samp=subsample)
+validate_y = resizer(validate_y, rows_val, batch_size, sub_samp=subsample)
 rows_tst = int(len(test_y) / batch_size / subsample)
-test_x = resizer(test_x, rows_tst, batch_size, subsample=subsample)
-test_y = resizer(test_y, rows_tst, batch_size, subsample=subsample)
-model=create_LSTM(hidden_units=hidden, dense_units=1, input_shape=(train_x.shape[1], train_x.shape[2]),
-                  activation=['relu', 'sigmoid', 'linear'], learning_rate=learning_rate, dropping=dropping,
-                  use_two=use_two)
+test_x = resizer(test_x, rows_tst, batch_size, sub_samp=subsample)
+test_y = resizer(test_y, rows_tst, batch_size, sub_samp=subsample)
+model = create_lstm(hidden_units=hidden, input_shape=(train_x.shape[1], train_x.shape[2]), dense_units=1,
+                    activation=['relu', 'sigmoid', 'linear'], learn_rate=learning_rate, drop=dropping,
+                    use_two_lstm=use_two)
 
 # Train model
 print("[INFO] training model...")
-epochs, mse, history = train_model(model, train_x, train_y, epochs_=epochs, batch_size=batch_size, verbose=1)
+epochs, mse, history = train_model(model, train_x, train_y, epochs_=epochs, btch_size=batch_size, verbose=1)
 plot_the_loss_curve(epochs, mse, history["loss"])
 
 # make predictions
@@ -205,15 +220,22 @@ validate_predict = model.predict(validate_x)
 test_predict = model.predict(test_x)
 
 # Print error
-print_error(trn_y=train_y[:,batch_size-1,:], val_y=validate_y[:,batch_size-1,:], tst_y=test_y[:,batch_size-1,:], trn_pred=train_predict, val_pred=validate_predict,
-            tst_pred=test_predict)
+print_error(trn_y=train_y[:, batch_size-1, :], val_y=validate_y[:, batch_size-1, :], tst_y=test_y[:, batch_size-1, :],
+            trn_pred=train_predict, val_pred=validate_predict, tst_pred=test_predict)
+
 # Plot result
 plot_input(trn_x=train_x, val_x=validate_x, tst_x=test_x)
-plot_result(trn_y=train_y[:,batch_size-1,:], val_y=validate_y[:,batch_size-1,:], tst_y=test_y[:,batch_size-1,:], trn_pred=train_predict, val_pred=validate_predict,
-            tst_pred=test_predict)
+plot_result(trn_y=train_y[:, batch_size-1, :], val_y=validate_y[:, batch_size-1, :], tst_y=test_y[:, batch_size-1, :],
+            trn_pred=train_predict, val_pred=validate_predict, tst_pred=test_predict)
+
+# Print model
+model.summary()
+print('\nShape:')
+for wt in model.layers[0].weights:
+    print(wt.name, '-->', wt.shape)
+
 plt.show()
 
-x = 1
 
 # Tb_boundaries = np.linspace(0, 50, 3)
 # inputs = {
