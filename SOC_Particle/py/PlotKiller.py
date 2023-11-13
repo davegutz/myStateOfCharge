a9730638194116d22e3f159c0bb83b0a85e909c1
Dave G <davegutz@alum.mit.edu>
2023-11-13 05:06:57 -0500

    Update PlotKiller.py

--- SOC_Particle/py/PlotKiller.py
+++ SOC_Particle/py/PlotKiller.py
@@ -20,21 +20,76 @@ import tkinter as tk
 
 
 class PlotKiller(tk.Toplevel):
-    def __init__(self, message, caller):
+    def __init__(self, message, caller, fig_list_=None):
         """Recursively keep asking to close plots until positive"""
+        self.fig_list = fig_list_
         tk.Toplevel.__init__(self)
         self.label = tk.Label(self, text=caller + ' ' + message)
         self.label.grid(row=0, column=0)
+        tk.Button(self, command=self.close_all, text="Close Figs " + caller).grid(row=2, column=0)
         self.lift()
-        tk.Button(self, command=self.close_all, text="Close All").grid(row=2, column=0)
         self.mainloop()
         # self.grab_set()  # Prevents other Tkinter windows from being used
 
     def close_all(self):
-        plt.close('all')
+        if self.fig_list is None:
+            plt.close('all')
+        else:
+            for fig in self.fig_list:
+                plt.close(fig)
         # self.grab_release()
         self.destroy()
 
 
-def show_killer(string, caller):
-    PlotKiller(string, caller)
+def show_killer(string, caller, fig_list=None):
+    PlotKiller(string, caller, fig_list)
+
+
+def simple_plot1():
+    import numpy as np
+    fig_list = []
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+    fig, ax = plt.subplots()
+    fig_list.append(fig)
+    ax.plot(t, s)
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+           title='Sine wave1')
+    ax.grid()
+    fig, ax = plt.subplots()
+    fig_list.append(fig)
+    ax.plot(t, s)
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+           title='Sine wave2')
+    ax.grid()
+    plt.show()
+    show_killer('close plots?', 'sp1', fig_list)
+
+
+def simple_plot2():
+    import numpy as np
+    fig_list = []
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+    fig, ax = plt.subplots()
+    fig_list.append(fig)
+    ax.plot(t, s)
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+           title='Sine wave1')
+    ax.grid()
+    fig, ax = plt.subplots()
+    fig_list.append(fig)
+    ax.plot(t, s)
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+           title='Sine wave2')
+    ax.grid()
+    plt.show()
+    show_killer('close plots?', 'sp2', fig_list)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    tk.Label(root, text="Try opening multiple plots then killing").pack()
+    tk.Button(root, text="plot 1", command=simple_plot1).pack()
+    tk.Button(root, text="plot 2", command=simple_plot2).pack()
+    root.mainloop()
