62ee0b1b976c27c212452d09674c57ec2881502c
Dave G <davegutz@users.noreply.github.com>
2023-11-13 15:29:00 -0500

    Update CountdownTimer.py

--- SOC_Particle/py/CountdownTimer.py
+++ SOC_Particle/py/CountdownTimer.py
@@ -20,6 +20,13 @@ import tkinter as tk
 from threading import Thread
 import pyautogui
 import time
+import platform
+if platform.system() == 'Darwin':
+    from ttwidgets import TTButton as myButton
+else:
+    import tkinter as tk
+    from tkinter import Button as myButton
+bg_color = "lightgray"
 
 
 class CountdownTimer(tk.Toplevel):
@@ -32,7 +39,9 @@ class CountdownTimer(tk.Toplevel):
         self.label = tk.Label(self, text=caller + ' ' + message)
         self.label.grid(row=0, column=0)
         self.lift()
-        tk.Button(self, command=self.countdown, text="START " + str(time_) + " sec timer").grid(row=2, column=0)
+        self.button = myButton(self, command=self.begin, text="START " + str(time_) + " sec timer")
+        self.button.grid(row=2, column=0)
+        self.center()
         self.mainloop()
         # self.grab_set()  # Prevents other Tkinter windows from being used
 
@@ -51,20 +60,91 @@ class CountdownTimer(tk.Toplevel):
         # self.grab_release()
         self.destroy()
 
+    def begin(self):
+        """Countdown in seconds then exit"""
+        thread = Thread(target=stay_awake, kwargs={'up_set_min': float(self.time.get()) / 60.})
+        thread.start()
+        self.lift()
+        self.center()
+        self.countdown()
+
     def countdown(self):
         """Countdown in seconds then exit"""
         msg = 'Counting down'
         self.time.set(self.time.get() - 1)
         self.label.config(text=f'{msg} ({self.time.get()}sec)')
+        self.button.config(text='', fg=bg_color, bg=bg_color)
         if self.time.get() > 0:
             self.lift()
             self.center()
             self.after(1000, self.countdown)
         else:
-            self.withdraw()
-            thread = Thread(target=stay_awake, kwargs={'up_set_min': float(self.time.get()) / 60.})
-            thread.start()
-            # R.record()  # this blocks.  'STOP' is used to end early
+            if message_window is None:  # window is not busy
+                message_start('flash')  # display message
+
+    def flash_bang(self):
+        while True:
+            print("before flash")
+            flash()
+            print("after flash")
+            self.after(10000)
+            print("before bell")
+            self.bell()
+            print("end")
+
+
+def flash():
+    flasher = tk.Toplevel()
+    flasher.geometry("300x250")
+    flasher.configure(bg='red')
+    flasher.deiconify()
+    flasher.after(2000, flasher.destroy())  # set the flash time to 100 milliseconds
+    flasher.mainloop()
+
+
+# function which creates window with message
+def message_start(text):
+    global repeates
+    global message_window
+    global message_label
+
+    repeates = 15  # how many times change background color
+
+    # create window with messages
+    message_window = tk.Toplevel()
+    message_window.geometry("300x250")
+    message_label = tk.Label(message_window, text=text, bg='red', fg='black')
+    message_label.pack()
+
+    # update window after 500ms
+    root.after(500, message_update)
+
+
+# function which changes background in displayed window
+def message_update():
+    global message_window
+    global repeates
+
+    if repeates > 0:
+        repeates -= 1
+
+        message_label['text'] = str(repeates)
+        if message_label['bg'] == 'red':
+            message_label['bg'] = 'white'
+            message_window.configure(bg='white')
+        else:
+            message_label['bg'] = 'red'
+            message_window.configure(bg='red')
+            message_label['bg'] = 'red'
+
+        # update window after 1000ms
+        root.after(1000, message_update)
+    else:
+        # close window
+        message_window.destroy()
+
+        # inform `check_time` that window is not busy
+        message_window = None
 
 
 def show_countdown_timer(time_, message, caller, exit_function=None):
@@ -91,10 +171,12 @@ def stay_awake(up_set_min=3.):
             pyautogui.press('shift')  # Shift key does not disturb fullscreen
         up_time_min = (time.time() - start_time) / 60.
         print(f"stay_awake: {up_time_min=}")
-    print(f"stay_awake: ending")
+    print(f"stay_awake: ending\n")
 
 
 if __name__ == '__main__':
+    message_window = None
+    repeates = 3
     root = tk.Tk()
     tk.Label(root, text="Try timer variations").pack()
     tk.Button(root, text="Timer", command=start).pack()
