eb2e3c0614ca2e6962e3ba10bdeec9bf040669a1
Dave G <davegutz@users.noreply.github.com>
2023-11-21 08:30:23 -0500

    Update version.h

--- SOC_Particle/src/version.h
+++ SOC_Particle/src/version.h
@@ -2,10 +2,6 @@
 #define version_h
 
 const String version = "g20231111";
-// g20231111 is g20230530d rolled over and hys bug fixed
-// g20230530d is g20230530d with Sh made non sp, Dw made sp, SG sp deleted, DEPLOY_PHOTON added, Serial1 help added
-// g20230530c is g20230530 with Mon.dv_hys = 0 and structurally deleted and vb_model_rev reversion
-// g20230530b is g20230530 with Mon.dv_hys = 0 and structurally deleted
-// g20230530a is g20230530 with Mon.dv_hys = 0
+// g20231111a (tab in GitHub) is g20231111 cleaned up for a rogue Talk.h function that was printing to stdout continuously
 
 #endif
