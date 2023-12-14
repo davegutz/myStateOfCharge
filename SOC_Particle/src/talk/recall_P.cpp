0a94f4c736ccf46a73c0c4bf1c4fe348eee8bef1
Dave G <davegutz@users.noreply.github.com>
2023-12-14 05:58:02 -0500

    followup

--- SOC_Particle/src/talk/recall_P.cpp
+++ SOC_Particle/src/talk/recall_P.cpp
@@ -41,89 +41,89 @@ boolean recall_P(const char letter, BatteryMonitor *Mon, Sensors *Sen)
     switch ( letter )
     {
         case ( 'a' ):  // Pa:  Print all
-        chit("Pm;", SOON);
-        chit("Ps;", SOON);
-        chit("Pr;", SOON);
-        chit("PM;", SOON);
-        chit("PN;", SOON);
-        chit("Ph;", SOON);
-        chit("Hd;", SOON);
-        chit("Pf;", SOON);
-        chit("Q;", SOON);
-        break;
+            chit("Pm;", SOON);
+            chit("Ps;", SOON);
+            chit("Pr;", SOON);
+            chit("PM;", SOON);
+            chit("PN;", SOON);
+            chit("Ph;", SOON);
+            chit("Hd;", SOON);
+            chit("Pf;", SOON);
+            chit("Q;", SOON);
+            break;
 
         case ( 'b' ):  // Pb:  Print Vb measure
-        Serial.printf("\nVolt:");   Serial.printf("Vb_bias_hdwe,Vb_m,mod,Vb=,%7.3f,%7.3f,%d,%7.3f,\n", 
-            sp.Vb_bias_hdwe(), Sen->Vb_model, sp.Modeling(), Sen->Vb);
-        break;
+            Serial.printf("\nVolt:");   Serial.printf("Vb_bias_hdwe,Vb_m,mod,Vb=,%7.3f,%7.3f,%d,%7.3f,\n", 
+                sp.Vb_bias_hdwe(), Sen->Vb_model, sp.Modeling(), Sen->Vb);
+            break;
 
         case ( 'e' ):  // Pe:  Print EKF
-        Serial.printf ("\nMon::"); Mon->EKF_1x1::pretty_print();
-        Serial1.printf("\nMon::"); Mon->EKF_1x1::pretty_print();
-        break;
+            Serial.printf ("\nMon::"); Mon->EKF_1x1::pretty_print();
+            Serial1.printf("\nMon::"); Mon->EKF_1x1::pretty_print();
+            break;
 
         case ( 'f' ):  // Pf:  Print faults
-        sp.print_history_array();
-        sp.print_fault_header();
-        sp.print_fault_array();
-        sp.print_fault_header();
-        Serial.printf ("\nSen::\n");
-        Sen->Flt->pretty_print (Sen, Mon);
-        Serial1.printf("\nSen::\n");
-        Sen->Flt->pretty_print1(Sen, Mon);
-        break;
+            sp.print_history_array();
+            sp.print_fault_header();
+            sp.print_fault_array();
+            sp.print_fault_header();
+            Serial.printf ("\nSen::\n");
+            Sen->Flt->pretty_print (Sen, Mon);
+            Serial1.printf("\nSen::\n");
+            Sen->Flt->pretty_print1(Sen, Mon);
+            break;
 
         case ( 'm' ):  // Pm:  Print mon
-        Serial.printf ("\nM:"); Mon->pretty_print(Sen);
-        Serial.printf ("M::"); Mon->Coulombs::pretty_print();
-        Serial.printf ("M::"); Mon->EKF_1x1::pretty_print();
-        Serial.printf ("\nmodeling %d\n", sp.Modeling());
-        break;
+            Serial.printf ("\nM:"); Mon->pretty_print(Sen);
+            Serial.printf ("M::"); Mon->Coulombs::pretty_print();
+            Serial.printf ("M::"); Mon->EKF_1x1::pretty_print();
+            Serial.printf ("\nmodeling %d\n", sp.Modeling());
+            break;
 
         case ( 'M' ):  // PM:  Print shunt Amp
-        Serial.printf ("\n"); Sen->ShuntAmp->pretty_print();
-        break;
+            Serial.printf ("\n"); Sen->ShuntAmp->pretty_print();
+            break;
 
         case ( 'N' ):  // PN:  Print shunt no amp
-        Serial.printf ("\n"); Sen->ShuntNoAmp->pretty_print();
-        break;
+            Serial.printf ("\n"); Sen->ShuntNoAmp->pretty_print();
+            break;
 
         case ( 'R' ):  // PR:  Print retained
-        Serial.printf("\n"); sp.pretty_print( true );
-        Serial.printf("\n"); sp.pretty_print( false );
-        break;
+            Serial.printf("\n"); sp.pretty_print( true );
+            Serial.printf("\n"); sp.pretty_print( false );
+            break;
 
         case ( 'r' ):  // Pr:  Print only off-nominal retained
-        Serial.printf("\n"); sp.pretty_print( false );
-        break;
+            Serial.printf("\n"); sp.pretty_print( false );
+            break;
 
         case ( 's' ):  // Ps:  Print sim
-        Serial.printf("\nmodeling=%d\n", sp.Modeling());
-        Serial.printf("S:");  Sen->Sim->pretty_print();
-        Serial.printf("S::"); Sen->Sim->Coulombs::pretty_print();
-        break;
+            Serial.printf("\nmodeling=%d\n", sp.Modeling());
+            Serial.printf("S:");  Sen->Sim->pretty_print();
+            Serial.printf("S::"); Sen->Sim->Coulombs::pretty_print();
+            break;
 
         case ( 'V' ):  // PV:  Print all volatile
-        Serial.printf("\n"); ap.pretty_print(true);
-        Serial.printf("\n"); cp.pretty_print();
-        Serial.printf("\n"); ap.pretty_print(false);
-        break;
+            Serial.printf("\n"); ap.pretty_print(true);
+            Serial.printf("\n"); cp.pretty_print();
+            Serial.printf("\n"); ap.pretty_print(false);
+            break;
 
         case ( 'v' ):  // Pv:  Print only off-nominal volatile
-        Serial.printf("\n"); ap.pretty_print(false);
-        break;
+            Serial.printf("\n"); ap.pretty_print(false);
+            break;
 
         case ( 'x' ):  // Px:  Print shunt measure
-        Serial.printf("\nAmp: "); Serial.printf("Vshunt_int,Vshunt,Vc,Vo,ib_tot_bias,Ishunt_cal=,%d,%7.3f,%7.3f,%7.3f,%7.3f,\n", 
-            Sen->ShuntAmp->vshunt_int(), Sen->ShuntAmp->vshunt(), Sen->ShuntAmp->Vc(), Sen->ShuntAmp->Vo(), Sen->ShuntAmp->Ishunt_cal());
-        Serial.printf("Noa:"); Serial.printf("Vshunt_int,Vshunt,Vc,Vo,ib_tot_bias,Ishunt_cal=,%d,%7.3f,%7.3f,%7.3f,%7.3f,\n", 
-            Sen->ShuntNoAmp->vshunt_int(), Sen->ShuntNoAmp->vshunt(), Sen->ShuntNoAmp->Vc(), Sen->ShuntNoAmp->Vo(), Sen->ShuntNoAmp->Ishunt_cal());
-        Serial.printf("Sel:Noa,Ib=,%d,%7.3f\n", sp.Ib_select(), Sen->Ib);
-        break;
+            Serial.printf("\nAmp: "); Serial.printf("Vshunt_int,Vshunt,Vc,Vo,ib_tot_bias,Ishunt_cal=,%d,%7.3f,%7.3f,%7.3f,%7.3f,\n", 
+                Sen->ShuntAmp->vshunt_int(), Sen->ShuntAmp->vshunt(), Sen->ShuntAmp->Vc(), Sen->ShuntAmp->Vo(), Sen->ShuntAmp->Ishunt_cal());
+            Serial.printf("Noa:"); Serial.printf("Vshunt_int,Vshunt,Vc,Vo,ib_tot_bias,Ishunt_cal=,%d,%7.3f,%7.3f,%7.3f,%7.3f,\n", 
+                Sen->ShuntNoAmp->vshunt_int(), Sen->ShuntNoAmp->vshunt(), Sen->ShuntNoAmp->Vc(), Sen->ShuntNoAmp->Vo(), Sen->ShuntNoAmp->Ishunt_cal());
+            Serial.printf("Sel:Noa,Ib=,%d,%7.3f\n", sp.Ib_select(), Sen->Ib);
+            break;
 
         default:
-        found = ap.find_adjust(cp.input_str) || sp.find_adjust(cp.input_str);
-        if (!found) Serial.printf("%s NOT FOUND\n", cp.input_str.substring(0,2).c_str());
+            found = ap.find_adjust(cp.input_str) || sp.find_adjust(cp.input_str);
+            if (!found) Serial.printf("%s NOT FOUND\n", cp.input_str.substring(0,2).c_str());
     }
     return found;
 }
--- SOC_Particle/src/talk/recall_R.cpp
+++ SOC_Particle/src/talk/recall_R.cpp
@@ -41,63 +41,63 @@ boolean recall_R(const char letter, BatteryMonitor *Mon, Sensors *Sen)
     switch ( letter )
     {
         case ( 'b' ):  // Rb:  Reset battery states (also hys)
-        Sen->Sim->init_battery_sim(true, Sen);  // Reset sim battery state
-        Mon->init_battery_mon(true, Sen);       // Reset mon battery state
-        break;
+            Sen->Sim->init_battery_sim(true, Sen);  // Reset sim battery state
+            Mon->init_battery_mon(true, Sen);       // Reset mon battery state
+            break;
 
         case ( 'f' ):  // Rf:  Reset fault Rf
-        Serial.printf("Reset latches\n");
-        Sen->Flt->reset_all_faults(true);
-        break;
+            Serial.printf("Reset latches\n");
+            Sen->Flt->reset_all_faults(true);
+            break;
 
         case ( 'i' ):  // Ri:  Reset infinite counter
-        Serial.printf("Reset infinite counter\n");
-        cp.inf_reset = true;
-        break;
+            Serial.printf("Reset infinite counter\n");
+            cp.inf_reset = true;
+            break;
 
         case ( 'r' ):  // Rr:  small reset counters
-        Serial.printf("CC reset\n");
-        Sen->Sim->apply_soc(1.0, Sen->Tb_filt);
-        Mon->apply_soc(1.0, Sen->Tb_filt);
-        cp.cmd_reset();
-        break;
+            Serial.printf("CC reset\n");
+            Sen->Sim->apply_soc(1.0, Sen->Tb_filt);
+            Mon->apply_soc(1.0, Sen->Tb_filt);
+            cp.cmd_reset();
+            break;
 
         case ( 'R' ):  // RR:  large reset
-        Serial.printf("RESET\n");
-        Serial1.printf("RESET\n");
-        Sen->Sim->apply_soc(1.0, Sen->Tb_filt);
-        Mon->apply_soc(1.0, Sen->Tb_filt);
-        cp.cmd_reset();
-        Sen->ReadSensors->delay(READ_DELAY);
-        Sen->Talk->delay(TALK_DELAY);
-        sp.large_reset();
-        sp.large_reset();
-        cp.large_reset();
-        cp.cmd_reset();
-        chit("HR;", SOON);
-        chit("Rf;", SOON);
-        chit("Hs;", SOON);
-        chit("Pf;", SOON);
-        break;
+            Serial.printf("RESET\n");
+            Serial1.printf("RESET\n");
+            Sen->Sim->apply_soc(1.0, Sen->Tb_filt);
+            Mon->apply_soc(1.0, Sen->Tb_filt);
+            cp.cmd_reset();
+            Sen->ReadSensors->delay(READ_DELAY);
+            Sen->Talk->delay(TALK_DELAY);
+            sp.large_reset();
+            sp.large_reset();
+            cp.large_reset();
+            cp.cmd_reset();
+            chit("HR;", SOON);
+            chit("Rf;", SOON);
+            chit("Hs;", SOON);
+            chit("Pf;", SOON);
+            break;
 
         case ( 's' ):  // Rs:  small reset filters
-        Serial.printf("reset\n");
-        cp.cmd_reset();
-        break;
+            Serial.printf("reset\n");
+            cp.cmd_reset();
+            break;
 
         case ( 'S' ):  // RS: renominalize saved pars
-        sp.set_nominal();
-        sp.pretty_print(true);
-        break;
+            sp.set_nominal();
+            sp.pretty_print(true);
+            break;
 
         case ( 'V' ):  // RV: renominalize volatile pars
-        ap.set_nominal();
-        ap.pretty_print(true);
-        break;
+            ap.set_nominal();
+            ap.pretty_print(true);
+            break;
 
         default:
-        found = ap.find_adjust(cp.input_str) || sp.find_adjust(cp.input_str);
-        if (!found) Serial.printf("%s NOT FOUND\n", cp.input_str.substring(0,2).c_str());
+            found = ap.find_adjust(cp.input_str) || sp.find_adjust(cp.input_str);
+            if (!found) Serial.printf("%s NOT FOUND\n", cp.input_str.substring(0,2).c_str());
     }
     return found;
 }
