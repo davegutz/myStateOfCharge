fb8ad51c88bf8784161e4eb9942233d691c541c3
Dave G <davegutz@alum.mit.edu>
2023-12-27 12:53:49 -0500

    linux setup debug

--- SOC_Particle/dataReduction/putty/puTTY_Forever_linux.sh
+++ SOC_Particle/dataReduction/putty/puTTY_Forever_linux.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # install this in /usr/local/bin with root priveledges
+# sudo cp puTTY_Forever_linux.sh /usr/local/bin
+# sudo chmod +777 /usr/local/bin/puTTY_Forever_linux.sh
+# at cli start putty; make def config - see puTTY_Windows_setup_def.odt
 while true
 do
 	nice --20 putty -load "def"
