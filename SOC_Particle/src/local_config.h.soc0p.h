147eb7d54b3c21fadacd43ab5a581d3c089a92b4
Dave G <davegutz@users.noreply.github.com>
2024-01-15 09:23:00 -0500

    finish soc0p cal

--- SOC_Particle/src/local_config.h.soc0p.h
+++ SOC_Particle/src/local_config.h.soc0p.h
@@ -24,15 +24,15 @@ const String unit = version + "_soc0p";
 #define MODELING              0   // Nominal modeling bitmap (* 'Xm'), 0=all hdwe, 1+=Tb, 2+=Vb, 4+=Ib, 7=all model.  +240 for discn
 
 // Sensor biases
-#define CURR_BIAS_AMP         0.973 // Calibration of amplified shunt sensor (* 'DA'), A
+#define CURR_BIAS_AMP         0.950 // Calibration of amplified shunt sensor (* 'DA'), A
 #define CURR_SCALE_AMP        1.006 // Hardware to match data (* 'SA')
-#define CURR_BIAS_NOA         0.150 // Calibration of non-amplified shunt sensor (* 'DB'), A
+#define CURR_BIAS_NOA         0.127 // Calibration of non-amplified shunt sensor (* 'DB'), A
 #define CURR_SCALE_NOA        1.042 // Hardware to match data (* 'SB')
 #define SHUNT_GAIN            1333. // Shunt V2A gain (scale with * 'SA' and 'SB'), A/V (-1333 is -100A/0.075V)
 #define SHUNT_AMP_R1          5600.     // Amplifed shunt ADS resistance, ohms
 #define SHUNT_AMP_R2          27000.    // Amplifed shunt ADS resistance, ohms
 #define CURR_BIAS_ALL         0.0   // Bias on all shunt sensors (* 'DI'), A
-#define VOLT_BIAS             0.03  // Bias on Vb sensor (* 'Dc'), V
+#define VOLT_BIAS             0.015 // Bias on Vb sensor (* 'Dc'), V
 #define TEMP_BIAS             0.0   // Bias on Tb sensor (* 'Dt'), deg C
 #define VB_SENSE_R_LO      4700      // Vb low sense resistor, ohm (4700)
 #define VB_SENSE_R_HI      20000     // Vb high sense resistor, ohm (20000)
