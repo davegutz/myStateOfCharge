d6c5363d89a6264b0953efb60d4c64c818d7d765
Dave G <davegutz@alum.mit.edu>
2024-03-21 17:07:31 -0400

    soc2p2

--- SOC_Particle/src/local_config.h
+++ SOC_Particle/src/local_config.h
@@ -1,7 +1,9 @@
 // #include "local_config.h.pro0p.h"
 // #include "local_config.h.pro1a.h"
-#include "local_config.h.pro3p2.h"
+// #include "local_config.h.pro3p2.h"
 // 
 // #include "local_config.h.pro2p2.h"
 // #include "local_config.h.soc0p.h"
 // #include "local_config.h.soc1a.h"
+#include "local_config.h.soc2p2.h"
+//#include "local_config.h.soc3p2.h"   failed.
