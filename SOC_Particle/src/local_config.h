c9c4f988041194844f9fd202ed14a1fc1eac28c2
Dave G <davegutz@alum.mit.edu>
2024-03-22 16:55:38 -0400

    pro3p2

--- SOC_Particle/src/local_config.h
+++ SOC_Particle/src/local_config.h
@@ -1,9 +1,9 @@
 // #include "local_config.h.pro0p.h"
 // #include "local_config.h.pro1a.h"
-// #include "local_config.h.pro3p2.h"
+#include "local_config.h.pro3p2.h"
 // 
-// #include "local_config.h.pro2p2.h"
+// #include "local_config.h.pro2p2.h"  // funny standalone breadboard like a DUE.  Avail. seldom used
 // #include "local_config.h.soc0p.h"
 // #include "local_config.h.soc1a.h"
-#include "local_config.h.soc2p2.h"
-//#include "local_config.h.soc3p2.h"   failed.
+//#include "local_config.h.soc3p2.h"   failed. shorted 3v3 to gnd with blob of solder
+// #include "local_config.h.soc2p2.h"
