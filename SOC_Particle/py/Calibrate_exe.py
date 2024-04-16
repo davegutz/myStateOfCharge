bdd6d1bc5132bbd7f152cf2408381cdd2a0e833d
Dave G <davegutz@alum.mit.edu>
2024-04-16 05:05:31 -0400

    status

--- SOC_Particle/py/Calibrate_exe.py
+++ SOC_Particle/py/Calibrate_exe.py
@@ -16,7 +16,7 @@
 """Executive function workflow to take some raw soc-voc data taken with assumed NOM_BATT_CAP = 100, plot it, allow user
 to enter fitted values of raw data, extract actual NOM_BATT_CAP then form it into final schedules.
 """
-from Battery import Battery, BatteryMonitor
+from Battery import Battery
 from unite_pictures import cleanup_fig_files
 from DataOverModel import write_clean_file
 import matplotlib.pyplot as plt
@@ -33,16 +33,16 @@ class DataC:
         self.data_file = data_file
         if data is None:
             self.soc = None
-            self.voc_soc = None
+            self.vstat = None
         else:
             self.i = 0
             self.soc = np.array(data.soc)
-            self.voc_soc = np.array(data.voc_soc)
+            self.vstat = np.array(data.vstat)
 
     def __str__(self):
         s = 'nom_unit_cap={:7.3f}\ntemp={:7.3f}\ndata_file={:s}\n soc, voc_soc,\n'.format(self.nom_unit_cap, self.temp_c, self.data_file)
         for i in np.arange(len(self.soc)):
-            s += "{:7.3f}, {:7.3f},\n".format(self.soc[i], self.voc_soc[i])
+            s += "{:7.3f}, {:7.3f},\n".format(self.soc[i], self.vstat[i])
         return s
 
 
@@ -51,31 +51,44 @@ class DataCP(DataC):
         DataC.__init__(self, nom_unit_cap, temp, None, data_file=data_file)
         if xdata is None or ydata is None:
             self.soc = None
-            self.voc_soc = None
+            self.vstat = None
         else:
             self.i = 0
             self.soc = np.array(xdata)
-            self.voc_soc = np.array(ydata)
+            self.vstat = np.array(ydata)
 
 
-def mash(raw_data):
+def mash(raw_data, v_off_thresh=Battery.VB_OFF):
     """Mash all data together normalized with same ordinates"""
+
+    # Aggregate the normalized soc data
     soc = []
+    soc_aggregate_off = 1.
     for (item, nom_unit_cap, temp, p_color, p_style, marker, marker_size) in raw_data:
-        for soc_data in item.soc:
+        for i in np.arange(len(item.soc)):
+            soc_data = item.soc[i]
+            vstat = item.vstat[i]
             soc_normal = normalize_soc(soc_data, nom_unit_cap, Battery.UNIT_CAP_RATED)
+            if vstat > v_off_thresh-1.:
+                soc_aggregate_off = min(soc_aggregate_off, soc_normal)
             soc.append(soc_normal)
     soc_sort = np.unique(np.array(soc))
+
+    # Mash
     mashed_data = []
     for (item, nom_unit_cap, temp, p_color, p_style, marker, marker_size) in raw_data:
         soc_normal = normalize_soc(item.soc, nom_unit_cap, Battery.UNIT_CAP_RATED)
-        lut_voc_soc = myTables.TableInterp1D(soc_normal, item.voc_soc)
+        lut_voc_soc = myTables.TableInterp1D(soc_normal, item.vstat)
         voc_soc_sort = []
         for soc in soc_sort:
             voc_soc_sort.append(lut_voc_soc.interp(soc))
         New_raw_data = DataCP(nom_unit_cap, item.temp_c, soc_sort, voc_soc_sort, item.data_file)
         mashed_data.append((New_raw_data, Battery.UNIT_CAP_RATED, temp, p_color, p_style, marker, marker_size))
-    return mashed_data
+
+    # Calculate actual capacity
+    actual_cap = (1.-soc_aggregate_off) * Battery.UNIT_CAP_RATED
+
+    return mashed_data, actual_cap
 
 
 def normalize_soc(soc_data, nom_unit_cap_data, unit_cap_rated=100.):
@@ -93,7 +106,7 @@ def plot_all_raw(raw_data, mashed_data, fig_files=None, plot_title=None, fig_lis
     plt.subplot(111)
     plt.title(plot_title + ' raw')
     for (item, nom_unit_cap, temp, p_color, p_style, marker, marker_size) in raw_data:
-        plt.plot(item.soc, item.voc_soc, color=p_color, linestyle=p_style, marker=marker, markersize=marker_size,
+        plt.plot(item.soc, item.vstat, color=p_color, linestyle=p_style, marker=marker, markersize=marker_size,
                  label='vstat ' + str(nom_unit_cap) + '  ' + str(temp) + 'C')
     plt.legend(loc=1)
     plt.xlabel('soc')
@@ -104,7 +117,7 @@ def plot_all_raw(raw_data, mashed_data, fig_files=None, plot_title=None, fig_lis
     plt.subplot(111)
     plt.title(plot_title + ' mashed')
     for (item, nom_unit_cap, temp, p_color, p_style, marker, marker_size) in mashed_data:
-        plt.plot(item.soc, item.voc_soc, color=p_color, linestyle=p_style, marker=marker, markersize=marker_size,
+        plt.plot(item.soc, item.vstat, color=p_color, linestyle=p_style, marker=marker, markersize=marker_size,
                  label='vstat ' + str(nom_unit_cap) + '  ' + str(temp) + 'C')
     plt.legend(loc=1)
     plt.xlabel('soc normal')
@@ -120,9 +133,12 @@ def plot_all_raw(raw_data, mashed_data, fig_files=None, plot_title=None, fig_lis
 
 def main():
 
-    data_files = [('G:/My Drive/GitHubArchive/SOC_Particle/dataReduction/g20240331/soc_voc 11C soc2p2_ch.csv', 100., 11., 'blue', '-.', 'o', 4),
-                  ('G:/My Drive/GitHubArchive/SOC_Particle/dataReduction/g20240331/soc_voc 21p5C soc2p2_ch.csv', 100., 21.5, 'orange', '--', 'x', 4),
-                  ('G:/My Drive/GitHubArchive/SOC_Particle/dataReduction/g20240331/soc_voc 25C soc2p2_ch.csv', 110., 25., 'red', '-', '1', 4)]
+    data_files = [('G:/My Drive/GitHubArchive/SOC_Particle/dataReduction/g20240331/soc_vstat 11C soc2p2_ch.csv',
+                   100., 11., 'blue', '-.', 'o', 4),
+                  ('G:/My Drive/GitHubArchive/SOC_Particle/dataReduction/g20240331/soc_vstat 21p5C soc2p2_ch.csv',
+                   100., 21.5, 'orange', '--', 'x', 4),
+                  ('G:/My Drive/GitHubArchive/SOC_Particle/dataReduction/g20240331/soc_vstat 25C soc2p2_ch.csv',
+                   110., 25., 'red', '-', '1', 4),]
     #  data_files[( data_file, nom_unit_cap, temp_c, p_color, marker, marker_size), (...), ...]  List of tuples of data from experiments
     #   data_file       Full path to data file
     #   nom_unit_cap    Nominal applied battery unit (100 Ah unit) capacity, Ah
@@ -133,7 +149,7 @@ def main():
     chm = 'ch'  # battery chemistry
     unit = 'soc2p2'  # Particle unit name
     unit_key = unit + '_' + chm  # key to be found in each line of data
-    title_key = 'voc_soc'  # key to be found in header for data
+    hdr_key = 'vstat'  # key to be found in header for data
 
     # Initialize
     raw_files = []
@@ -142,7 +158,7 @@ def main():
     fig_files = []
     data_file_clean = None
     for (data_file, nom_unit_cap, temp, p_color, p_style, marker, marker_size) in data_files:
-        data_file_clean = write_clean_file(data_file, type_='', title_key=title_key, unit_key=unit_key)
+        data_file_clean = write_clean_file(data_file, type_='', hdr_key=hdr_key, unit_key=unit_key)
         if data_file_clean is None:
             print(f"case {data_file=} {temp=} is dirty...skipping")
             continue
@@ -153,13 +169,17 @@ def main():
             Raw_files.append((Raw_data, nom_unit_cap, temp, p_color, p_style, marker, marker_size))
 
     # Mash all the data to one table and normalize to NOM_UNIT_CAP = 100
-    Mashed_data = mash(Raw_files)
+    Mashed_data, actual_nom_unit_cap = mash(Raw_files)
+    print("\nAfter mashing:  capacity = {:5.1f}".format(actual_nom_unit_cap))
+
+    # Shift and scale curves for calculated capacity
 
     date_time = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
     data_root = data_file_clean.split('/')[-1].replace('.csv', '_')
     filename = data_root + sys.argv[0].split('/')[-1].split('\\')[-1].split('.')[-2]
     plot_title = filename + '   ' + date_time
-    plot_all_raw(Raw_files, Mashed_data, fig_files=fig_files, plot_title=plot_title, fig_list=fig_list, filename='Calibrate_exe')
+    plot_all_raw(Raw_files, Mashed_data, fig_files=fig_files, plot_title=plot_title, fig_list=fig_list,
+                 filename='Calibrate_exe')
     plt.show()
     cleanup_fig_files(fig_files)
 
