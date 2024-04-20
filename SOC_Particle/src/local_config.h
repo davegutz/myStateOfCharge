28fa72921ba486cf0341d6660888717050ae5650
Dave G <davegutz@alum.mit.edu>
2024-04-20 12:22:19 -0400

    Update local_config.h

--- SOC_Particle/src/local_config.h
+++ SOC_Particle/src/local_config.h
@@ -12,25 +12,24 @@
 // #include "local_config.h.soc2p2.h"
 
 #ifdef CONFIG_PRO0P
-    #define _UNIT str("pro0p")
+    const String unit = version + "_pro0p";
 #elif defined(CONFIG_PRO1A)
-    #define _UNIT str("pro1a")
+    const String unit = version + "_pro1a";
 #elif defined(CONFIG_PRO2P2)
-    #define _UNIT str("pro2p2")
+    const String unit = version + "_pro2p2";
 #elif defined(CONFIG_PRO3P2)
-    #define _UNIT str("pro3p2")
+    const String unit = version + "_pro3p2";
 #elif defined(CONFIG_SOC0P)
-    #define _UNIT str("soc0p")
+    const String unit = version + "_soc0p";
 #elif defined(CONFIG_SOC1A)
-    #define _UNIT str("soc1a")
+    const String unit = version + "_soc1a";
 #elif defined(CONFIG_SOC2P2)
-    #define _UNIT str("soc2p2")
+    const String unit = version + "_soc2p2";
 #elif defined(CONFIG_SOC3P2)
-    #define _UNIT str("soc3p2")
-#else
+    const String unit = version + "_soc3p2";
+#els
     #error "CONFIG_<UNIT> not defined"
 #endif
 
-const String unit = version + "_" + _UNIT;
 
 #endif
