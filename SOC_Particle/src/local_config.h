d172054fd05e42342527b08fa3a25a34c44cc0a0
Dave G <davegutz@alum.mit.edu>
2024-04-21 12:08:40 -0400

    Update local_config.h

--- SOC_Particle/src/local_config.h
+++ SOC_Particle/src/local_config.h
@@ -11,25 +11,4 @@
 // //#include "local_config.h.soc3p2.h"   failed. shorted 3v3 to gnd with blob of solder
 // #include "local_config.h.soc2p2.h"
 
-#ifdef CONFIG_PRO0P
-    const String unit = version + "_pro0p";
-#elif defined(CONFIG_PRO1A)
-    const String unit = version + "_pro1a";
-#elif defined(CONFIG_PRO2P2)
-    const String unit = version + "_pro2p2";
-#elif defined(CONFIG_PRO3P2)
-    const String unit = version + "_pro3p2";
-#elif defined(CONFIG_SOC0P)
-    const String unit = version + "_soc0p";
-#elif defined(CONFIG_SOC1A)
-    const String unit = version + "_soc1a";
-#elif defined(CONFIG_SOC2P2)
-    const String unit = version + "_soc2p2";
-#elif defined(CONFIG_SOC3P2)
-    const String unit = version + "_soc3p2";
-#els
-    #error "CONFIG_<UNIT> not defined"
-#endif
-
-
 #endif
