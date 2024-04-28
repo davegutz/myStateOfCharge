fbfbb4f2f4df7f44a443c5e886c3ec42a6fd6eab
Dave G <davegutz@alum.mit.edu>
2024-04-28 14:36:07 -0400

    Update local_config.h.soc3p2.h

--- SOC_Particle/src/local_config.h.soc3p2.h
+++ SOC_Particle/src/local_config.h.soc3p2.h
@@ -45,7 +45,7 @@
 //   would have  NOM_UNIT_CAP 200, NS 2, and NP 2
 #define COULOMBIC_EFF_SCALE   1.0     // Scalar on Coulombic efficiency of battery, fraction of charge that gets used (1.0)
 #define CHEM                  2       // Chemistry monitor code integer, 0=Battleborn, 1=CHINS-guest room, 2=CHINS-garage-guest room, 2=CHINS-garage
-#define NOM_UNIT_CAP          101.6   // Nominal battery unit capacity.  (* 'Sc' or '*BS'/'*BP'), Ah
+#define NOM_UNIT_CAP          102.9   // Nominal battery unit capacity.  (* 'Sc' or '*BS'/'*BP'), Ah
 #define HYS_SCALE             1.0     // Scalar on hysteresis (1.0)
 #define NS                    2.0     // Number of series batteries in bank.  Fractions scale and remember NOM_UNIT_CAP (* 'BS')
 #define NP                    2.0     // Number of parallel batteries in bank.  Fractions scale and remember NOM_UNIT_CAP (* 'BP')
