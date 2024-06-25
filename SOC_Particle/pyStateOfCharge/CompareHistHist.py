22e4697dcc6600f175cfd92a31ccd974a05f60c6
Dave G <davegutz@alum.mit.edu>
2024-06-25 17:16:16 -0400

    cleanup

--- SOC_Particle/pyStateOfCharge/CompareHistHist.py
+++ SOC_Particle/pyStateOfCharge/CompareHistHist.py
@@ -15,18 +15,11 @@
 
 """ Slice and dice the history dumps."""
 
-import numpy as np
-import numpy.lib.recfunctions as rf
 import matplotlib.pyplot as plt
-from Hysteresis_20220917d import Hysteresis_20220917d
-from Hysteresis_20220926 import Hysteresis_20220926
-from Battery import Battery, BatteryMonitor, is_sat, Retained
 from PlotKiller import show_killer
 from DataOverModel import dom_plot
 from PlotGP import tune_r, gp_plot
 from PlotOffOn import off_on_plot
-from Chemistry_BMS import ib_lag
-from myFilters import LagExp
 from unite_pictures import unite_pictures_into_pdf, cleanup_fig_files, precleanup_fig_files
 from datetime import datetime
 from local_paths import version_from_data_file, local_paths
@@ -38,100 +31,6 @@ if sys.platform == 'darwin':
     import matplotlib
     matplotlib.use('tkagg')
 plt.rcParams['axes.grid'] = True
-
-#  For this battery Battleborn 100 Ah with 1.084 x capacity
-IB_BAND = 1.  # Threshold to declare charging or discharging
-TB_BAND = 25.  # Band around temperature to group data and correct.  Large value means no banding, effectively
-HYS_SCALE_20220917d = 0.3  # Original hys_remodel scalar inside photon code
-HYS_SCALE_20220926 = 1.0  # Original hys_remodel scalar inside photon code
-
-#  Rescale parameters design.  Minimal tuning attempt
-#  This didn't work because low soc response of original design is too slow
-HYS_RESCALE_CHG = 0.5  # Attempt to rescale to match voc_soc to all data
-HYS_RESCALE_DIS = 0.3  # Attempt to rescale to match voc_soc to all data
-VOC_RESET_05 = 0.  # Attempt to rescale to match voc_soc to all data
-VOC_RESET_11 = 0.  # Attempt to rescale to match voc_soc to all data
-VOC_RESET_20 = 0.  # Attempt to rescale to match voc_soc to all data
-VOC_RESET_30 = -0.03  # Attempt to rescale to match voc_soc to all data
-VOC_RESET_40 = 0.  # Attempt to rescale to match voc_soc to all data
-
-#  Redesign Hysteresis_20220917d.  Make a new Hysteresis_20220926.py with new curve
-HYS_CAP_REDESIGN = 3.6e4  # faster time constant needed
-HYS_SOC_MIN_MARG = 0.15  # add to soc_min to set thr for detecting low endpoint condition for reset of hysteresis
-
-
-# Table lookup vector
-def look_it(x, tab, temp):
-    voc = x.copy()
-    for i in range(len(x)):
-        voc[i] = tab.interp(x[i], temp)
-    return voc
-
-
-def shift_time(mo, extra_shift=0.):
-    # Shift time
-    first_non_zero = 0
-    n = len(mo.time)
-    while abs(mo.ib[first_non_zero]) < 0.02 and first_non_zero < n-1:
-        first_non_zero += 1
-    if first_non_zero < n:  # success
-        if first_non_zero > 0:
-            shift = (mo.time[first_non_zero] + mo.time[first_non_zero-1]) / 2.
-        else:
-            shift = mo.time[first_non_zero]
-        print('shift time by', shift)
-        mo.time = mo.time - shift + extra_shift
-    return mo
-
-
-def add_chm(hist, mon_t_=False, mon=None, chm=None):
-    if mon_t_ is False or mon is None:
-        print("add_chm:  not executing")
-        if chm is not None:
-            chm_s = []
-            for i in range(len(hist.time)):
-                chm_s.append(chm)
-            hist = rf.rec_append_fields(hist, 'chm_s', np.array(chm_s, dtype=int))
-            hist = rf.rec_append_fields(hist, 'chm', np.array(chm_s, dtype=int))
-        return hist
-    else:
-        chm = []
-        for i in range(len(hist.time_ux)):
-            t_sec = float(hist.time_ux[i] - hist.time_ux[0]) + mon.time[0]
-            chm.append(np.interp(t_sec, mon.time, mon.chm))
-        hist = rf.rec_append_fields(hist, 'chm_s', np.array(chm, dtype=int))
-        hist = rf.rec_append_fields(hist, 'chm', np.array(chm, dtype=int))
-    return hist
-
-
-def add_mod(hist, mon_t_=False, mon=None):
-    if mon_t_ is False or mon is None:
-        print("add_mod:  not executing")
-        return hist
-    else:
-        mod_data = []
-        for i in range(len(hist.time_ux)):
-            t_sec = float(hist.time_ux[i]) - float(hist.time_ux[0]) + mon.time[0]
-            mod_data.append(np.interp(t_sec, mon.time, mon.mod_data))
-        return rf.rec_append_fields(hist, 'mod_data', np.array(mod_data, dtype=int))
-
-
-def add_qcrs(hist, mon_t_=False, mon=None, qcrs=None):
-    if mon_t_ is False or mon is None:
-        print("add_qcrs:  not executing")
-        if qcrs is not None:
-            qcrs_m = []
-            for i in range(len(hist.time)):
-                qcrs_m.append(qcrs)
-            hist = rf.rec_append_fields(hist, 'qcrs_s', np.array(qcrs_m, dtype=float))
-            hist = rf.rec_append_fields(hist, 'qcrs', np.array(qcrs_m, dtype=float))
-        return hist
-    else:
-        qcrs_m = []
-        for i in range(len(hist.time_ux)):
-            t_sec = float(hist.time_ux[i] - hist.time_ux[0]) + mon.time[0]
-            qcrs_m.append(np.interp(t_sec, mon.time, mon.chm))
-    return hist
 
 
 def compare_hist_hist(data_file_ref=None, unit_key_ref=None, data_file_tst=None, unit_key_tst=None,
