9e79a50eeffa7fdcb3d248d4bdcc3a8d24291b26
Dave G <davegutz@alum.mit.edu>
2024-06-29 08:21:45 -0400

    flash soc2p2 garage

--- SOC_Particle/src/pro3p2.h
+++ SOC_Particle/src/pro3p2.h
@@ -21,22 +21,22 @@
 #define MODELING              2   // Nominal modeling bitmap (* 'Xm'), 0=all hdwe, 1+=Tb, 2+=Vb, 4+=Ib, 7=all model.  +240 for discn
 
 // Sensor biases
-#define CURR_BIAS_AMP         0.10  // Calibration of amplified shunt sensor (* 'DA'), A
+#define CURR_BIAS_AMP         0.00  // Calibration of amplified shunt sensor (* 'DA'), A
 #define CURR_SCALE_AMP         1.0  // Hardware to match data (* 'SA')
-#define CURR_BIAS_NOA         0.25  // Calibration of non-amplified shunt sensor (* 'DB'), A
+#define CURR_BIAS_NOA         0.00  // Calibration of non-amplified shunt sensor (* 'DB'), A
 #define CURR_SCALE_NOA         1.0  // Hardware to match data (* 'SB')
 #define CURR_SCALE_DISCH       1.0  // Scale discharge to account for asymetric inverter action only on discharge (* 'SD'), slr
-#define SHUNT_GAIN            1333. // Shunt V2A gain (scale with * 'SA' and 'SB'), A/V (1333 is 100A/0.075V)
+#define SHUNT_GAIN            2666. // Shunt V2A gain (scale with * 'SA' and 'SB'), A/V (1333 is 100A/0.075V)
 #define SHUNT_AMP_R1          5100. // Internal amp resistance 196x, ohms (5100)
 #define SHUNT_AMP_R2       1000000. // Internal amp resistance 196x, ohms (1000000)
-#define IB_ABS_MAX_AMP        12.0  // Hard range limit of sensor electrically impossible (=1.65 * SHUNT_GAIN * SHUNT_AMP_R1 / SHUNT_AMP_R2 *1.05) but saw -11.48 A
+#define IB_ABS_MAX_AMP        24.5  // Hard range limit of sensor electrically impossible (=1.65 * SHUNT_GAIN * SHUNT_AMP_R1 / SHUNT_AMP_R2 *1.05) but saw -11.48 A
 #define SHUNT_NOA_R1          5100. // Internal amp resistance 29.4x, ohms (5100)
 #define SHUNT_NOA_R2         75000. // Internal amp resistance 29.4x, ohms (varies)
-#define IB_ABS_MAX_NOA         157. // Hard range limit of sensor electrically impossible (=1.65 * SHUNT_GAIN * SHUNT_NOA_R1 / SHUNT_NOA_R2 *1.05)
-#define HDWE_IB_HI_LO_NOA_LO   -11. // Full NOA discharge transition, A (-11)
-#define HDWE_IB_HI_LO_AMP_LO   -10. // Full AMP discharge transition, A (-10)  
-#define HDWE_IB_HI_LO_AMP_HI    10. // Full AMP charge transition, A (10)
-#define HDWE_IB_HI_LO_NOA_HI    11. // Full NOA charge transition, A (11)
+#define IB_ABS_MAX_NOA         314. // Hard range limit of sensor electrically impossible (=1.65 * SHUNT_GAIN * SHUNT_NOA_R1 / SHUNT_NOA_R2 *1.05)
+#define HDWE_IB_HI_LO_NOA_LO   -22. // Full NOA discharge transition, A (-11)
+#define HDWE_IB_HI_LO_AMP_LO   -20. // Full AMP discharge transition, A (-10)  
+#define HDWE_IB_HI_LO_AMP_HI    20. // Full AMP charge transition, A (10)
+#define HDWE_IB_HI_LO_NOA_HI    22. // Full NOA charge transition, A (11)
 #define CURR_BIAS_ALL           0.0 // Bias on all shunt sensors (* 'DI'), A
 #define VOLT_BIAS              0.00 // Bias on Vb sensor (* 'Dc'), V
 #define TEMP_BIAS               0.0 // Bias on Tb sensor (* 'Dt'), deg C
