1487fa80533689b3d22d0865208c92194db5acaa
Dave G <davegutz@alum.mit.edu>
2024-08-04 12:27:06 -0400

    Update local_config.h

--- SOC_Particle/src/local_config.h
+++ SOC_Particle/src/local_config.h
@@ -5,12 +5,12 @@
 // #include "pro0p_chg.h"
 // #include "pro1a.h"
 // #include "pro2p2_hi_lo.h"
-#include "pro3p2.h"
+// #include "pro3p2.h"
 // #include "pro3p2_hi_lo.h"
 // #include "soc0p.h"  // Guest room (old)
 // #include "soc1a.h"  // Truck (old)
 // #include "soc2p2_hi_lo.h"  // Garage
 // #include "soc3p2_hi_lo.h"  // Guest room
-// #include "soc4p2_hi_lo.h"  // Truck
+#include "soc4p2_hi_lo.h"  // Truck 154/105
 
 #endif
