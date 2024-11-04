ef788616e312edd770e024baab84b91e0a002816
Dave G <davegutz@alum.mit.edu>
2024-11-04 05:09:25 -0500

    Update constants.h

--- SOC_Particle/src/constants.h
+++ SOC_Particle/src/constants.h
@@ -239,7 +239,7 @@ const float QUIET_R   (QUIET_S/10.);    // Quiet reset persistence, sec ('up 1 d
 #define HALF_V3V3         (V3V3/2.)     // Theoretical center of differential TSC2010
 #define USE_SH_2WIRE                    // Use Steinhart-Hart 2-wire temperature characteristic when defined
 #define HDWE_RS_2WIRE   15000.          // 2-wire sense resistor, ohm (15000.)
-#define HDWE_M_2WIRE    -58.96          // 2-wire thermistor characteristic, data fit (-58.96; see '2-wireRTD.ods')  TODO: switch over to industry accepted exponential equation Steinhart-Hart (see '2-wireRTD.ods')
+#define HDWE_M_2WIRE    -58.96          // 2-wire thermistor characteristic, data fit (-58.96; see '2-wireRTD.ods')
 #define HDWE_B_2WIRE    262.79          // 2-wire thermistor characteristic, data fit (262.79; see '2-wireRTD.ods')
 #define HDWE_SHA_2WIRE  9.8194e-4       // 2-wire thermistor characteristic, Steinhart-Hart (9.8194e-4; see '2-wireRTD.ods')
 #define HDWE_SHB_2WIRE  2.4775e-4       // 2-wire thermistor characteristic, Steinhart-Hart (2.4775e-4; see '2-wireRTD.ods')
