ab6c29d4ffe00c3ec6feea6902482cc73b64397a
Dave G <davegutz@alum.mit.edu>
2024-12-04 13:05:43 -0500

    print SAT for vv5

--- SOC_Particle/src/subs.cpp
+++ SOC_Particle/src/subs.cpp
@@ -349,10 +349,13 @@ void oled_display(Adafruit_SSD1306 *display, Sensors *Sen, BatteryMonitor *Mon)
   else if ( sp.debug()==98 ) // Calibration mode
     debug_98(Mon, Sen);
   else if ( sp.debug()!=-2 )  // Normal display
+  {
     Serial1.printf("%s   Tb,C  VOC,V  Ib,A \n%s   EKF,Ah  chg,hrs  CC, Ah\nPf; for fails.  prints=%ld\n\n",
       disp_Tbop.c_str(), dispBot.c_str(), cp.num_v_print);
-
-  if ( sp.debug()==5 ) debug_5(Mon, Sen);  // Charge time display on UART
+      if ( sp.debug()==5 )
+        Serial.printf("Tb,C  VOC,V  Ib,A, EKF,Ah  chg,hrs  CC, Ah  %s , %s\n",
+          disp_Tbop.c_str(), dispBot.c_str());
+  }
 
   blink += 1;
   if (blink>3) blink = 0;
@@ -500,10 +503,13 @@ void oled_display(Sensors *Sen, BatteryMonitor *Mon)
   else if ( sp.debug()==98 ) // Calibration mode
     debug_98(Mon, Sen);
   else if ( sp.debug()!=-2 )  // Normal display
-    Serial1.printf("%s   Tb,C  VOC,V  Ib,A \n%s   EKF,Ah  chg,hrs  CC, Ah\nPf; for fails.  prints=%ld\n\n",
-      disp_Tbop.c_str(), dispBot.c_str(), cp.num_v_print);
-
-  if ( sp.debug()==5 ) debug_5(Mon, Sen);  // Charge time display on UART
+    {
+      Serial1.printf("%s   Tb,C  VOC,V  Ib,A \n%s   EKF,Ah  chg,hrs  CC, Ah\nPf; for fails.  prints=%ld\n\n",
+        disp_Tbop.c_str(), dispBot.c_str(), cp.num_v_print);
+      if ( sp.debug()==5 )
+        Serial.printf("Tb,C  VOC,V  Ib,A, EKF,Ah  chg,hrs  CC, Ah  %s , %s\n",
+          disp_Tbop.c_str(), dispBot.c_str());
+    }
 
   blink += 1;
   if (blink>3) blink = 0;
